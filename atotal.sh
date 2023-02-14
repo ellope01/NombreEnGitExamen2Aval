b13f9509bbd950ce0cc485fc85313d88bc206d4e
Elizabeth Lopez <ellope01@floridauniversitaria.es>
2023-02-14 15:51:40 +0100

    atotal

---
+++ atotal.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+read -p "Escriba el nombre de la ciudad: " ciudad
+echo "El consumo total de la ciudad seleccionada es de "
+numFich= cat consumo.txt | grep $ciudad | awk '{ sum += $4 } END { print sum }' 
+
